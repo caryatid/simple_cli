bafaaaa7a54b48e9e364be9521d34d9ba4c8b4d5
Dave Setchell <setchell.dave@gmail.com>
2016-05-04 19:02:11 -0700

    fixes

--- cli.py
+++ cli.py
@@ -2,6 +2,7 @@
 import sys
 import os
 from itertools import zip_longest, product
+from collections import defaultdict
         
 def _match(possible, m=None):
     acts = set() if m else possible
@@ -14,49 +15,117 @@ def matching(possible, m=None):
     else:
         args = []
         current.update(possible)
-    [(new.add(x[1:]), args.remove(x)) for x in list(args) if x[0] == '_']
     [new.add(x) for x in args if not _match(possible, x)]
     [current.update(_match(possible, x)) for x in args]
     return (new, current)
 
 def handle(commands, args=None):
-    a = args.pop(0) if args else []
-    n_cmds, cmds = matching(commands.keys(), a)
-    for com in cmds:
-        process = zip_longest(args, commands[com][1], fillvalue='')
-        process = map(lambda x: (x[0], x[1][0], x[1][1], x[1][2]), process)
-        params = []
-        for arg, group, possible, maker in process:
-            if arg and arg[0] == '~':
-                arg = arg[1:]
-                n, c = matching(files_comps(), arg)
-            else:
-                n, c = matching(possible(), arg)
-            if group == 'both':
-                pos = n | c
-            elif group == 'new':
-                pos = n
-            elif group == 'current':
-                pos = c
-            params.append([maker(x) for x in pos])
-        for variation in product(*params):
-            yield commands[com][0](*variation)
+    a = args.pop(0) if args else ''
+    dry = False
+    if a and a.startswith('_'):
+        a = a[1:]
+        dry = True
+    _, cmds = matching(commands.keys(), a)
+    if len(cmds) == 0:
+        args = []
+        if 'help' not in commands:
+            commands.update({'help': (lambda : 'available commands:\n' +
+                                      '\n'.join(commands.keys()), [])})
+        cmds = set(['help'])
+    com_name = sorted(cmds, key=lambda x: len(x))[0]
+    if dry:
+        cmd = lambda *x: list(map(str, x))
+    else:
+        cmd = commands[com_name][0]
+    arg_h = commands[com_name][1]
+    params = []
+    procs = map(lambda y: (y[0], y[1][0], y[1][1], y[1][2]),
+                zip_longest(args, arg_h, fillvalue=''))
+    for arg, group, possible, maker in procs:
+        if arg and arg[0] == '+':
+            arg = arg[1:]
+            n, c = matching(_stdin_list(), arg)
+        elif arg and arg[0] == '.':
+            arg = arg[1:]
+            n, c = matching(files_comps(), arg)
+        elif arg and arg[0] == ',':
+            arg = arg[1:]
+            n = set(arg.split(','))
+            c = n
+        else:
+            n, c = matching(possible(params), arg)
+        if 'current' in group:
+            pos = c
+        elif 'new' in group:
+            pos = n
+        else:
+            pos = n | c
+        if '1' in group and not arg:
+            pos = set()
+        params.append([y for y in [maker(x) for x in pos] if y])
+        params = [x for x in params if x]
+    for variation in product(*params):
+        yield cmd(*variation)
 
+def t_to_d(ts):
+    ts = list(ts)
+    if not ts or not isinstance(ts[0], list):
+        return {'return': ts}
+    # takes list of tuples all of same length.
+    # returns dictionary
+    # determine if mappend or assign final value
+    l = ['.'.join(map(str, x[:-1])) for x in ts]
+    s = set(l)
+    assign = True
+    if len(l) > len(s):
+        assign = False
+    d = {}
+    for t in ts:
+        _d = d
+        k = t.pop(0)
+        while t:
+            _k = t.pop(0)
+            if not t:
+                if assign:
+                    _d[k] = _k
+                else:
+                    _d.setdefault(k, list())
+                    _d[k].append(_k)
+            else:
+                _d.setdefault(k, {})
+                _d = _d[k]
+                k = _k
+    return d
+            
 def _file_list(top=None):
     top = '.' if not top else top
     for p, _, fs in os.walk(top):
         for f in fs:
             yield os.path.join(p, f)
         
-files_comps = lambda: set(_file_list())
+
+def _stdin_list():
+    return [x.strip() for x in sys.stdin.readlines()]
+
+def none_or_one(s):
+    if len(s) >= 1:
+        return sorted(s, key=len)[:1]
+    else:
+        return [None]
+
+files_p = lambda: set(_file_list())
+stind_p = lambda: set(_stdin_list())
+raw_p = ('both', lambda: set(['']), lambda x: x)
+
+
 
 if __name__ == '__main__':
     # example
+    import yaml
     data_1 = set('this old man he played dumb he played tic tac'.split())
     data_2 = set('we all have time for our friends but we spend it instead'.split())
+    data_3 = set('lastly for another time and place'.split())
     sys.argv.pop(0)
-    cmds = {'one': (lambda a, b: (a, b), [('both', lambda: data_1, lambda x: x.upper()),
-                                          ('current', lambda: data_2, lambda x: x.upper())]),
-            'two': (lambda a, b: ('two', a, b), [('both', lambda: data_1, lambda x: x.upper()),    
-                                          ('current', lambda: data_2, lambda x: x.upper())])}
-    print(list(handle(cmds, sys.argv)))
+    cmds = {'one': (lambda a, b: [a, b], [('current', lambda x: data_1, lambda x: x.upper()),
+                                          ('both', lambda x: data_2, lambda x: x.upper())])}
+    print(yaml.dump(t_to_d(handle(cmds, sys.argv))))
